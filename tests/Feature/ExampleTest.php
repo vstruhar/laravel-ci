3bdd3b55501f2e0c0b9e04237d0aa464f767c0ae
Vladimir Struhar <vstruhar@gmail.com>
2024-04-12 11:17:07 +0200

    testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 it('returns a successful response', function () {
     $response = $this->get('/');
 
     $response->assertStatus(200);
+});
+
+it('should be able to use database', function () {
+    $user = User::create([
+        'name' => $name = fake()->firstName(),
+        'email' => $email = fake()->email(),
+        'password' => Hash::make('password'),
+    ]);
+
+    $this->assertDatabaseHas('users', [
+        'name' => $name,
+        'email' => $email,
+    ]);
+
+    $this->assertDatabaseCount('users', 1);
+
+    expect($name)->toBe($user->name);
 });
